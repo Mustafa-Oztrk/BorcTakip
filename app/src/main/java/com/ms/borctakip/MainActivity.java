01895458c3bcf40670787b59cc1db97903fa685a
Mustafa-Oztrk <ozturkmustafa174@gmail.com>
2024-07-24 13:44:41 +0300

    Initial commit

---
+++ app/src/main/java/com/ms/borctakip/MainActivity.java
@@ -0,0 +1,45 @@
+package com.ms.borctakip;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return WindowInsetsCompat.CONSUMED; // or you can return 'insets' based on your needs
+        });
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.borc_menu);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.addDebt) {
+                    Intent i = new Intent(getApplicationContext(), AddDebtActivity.class);
+                    startActivity(i);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
